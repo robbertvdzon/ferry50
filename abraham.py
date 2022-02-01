6ebc4f53bd0add3da7e4c49ed4c0f54eff753717
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-01 16:42:49 +0100

    Update app

--- abraham.py
+++ abraham.py
@@ -4,10 +4,14 @@ kit = ServoKit(channels=16)
 middle = 75
 reach = 35
 delay = 1.0
+longdelay = 4.0
+numberOfTypeMovements = 6
 while(True):
+    for x in range(numberOfTypeMovements):
         kit.servo[0].angle = middle - reach
-        kit.servo[15].angle = middle + reach
-        time.sleep(delay)
-        kit.servo[0].angle = middle + reach
         kit.servo[15].angle = middle - reach
         time.sleep(delay)
+        kit.servo[0].angle = middle + reach
+        kit.servo[15].angle = middle + reach
+        time.sleep(delay)
+    time.sleep(longdelay)
