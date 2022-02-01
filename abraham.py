54b8c19a80fb946ce92ff3a59b0831af0bdc1b96
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-01 16:20:58 +0100

    Update app

--- abraham.py
+++ abraham.py
@@ -1,8 +1,10 @@
 import time
 from adafruit_servokit import ServoKit
-kit = ServoKit(channels=8)
+kit = ServoKit(channels=16)
 while(True):
         kit.servo[0].angle = 40
-        time.sleep(0.3)
+        kit.servo[15].angle = 40
+        time.sleep(1)
         kit.servo[0].angle = 0
-        time.sleep(0.3)
+        kit.servo[15].angle = 0
+        time.sleep(1)
---
+++ start.sh
@@ -0,0 +1,2 @@
+/usr/bin/python /home/pi/ferry50/abraham.py&
+/usr/bin/vlc --loop --fullscreen /home/pi/ferry3.avi
