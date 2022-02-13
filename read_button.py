8e11d3887f1fc5532cd60be2e6eb23cbcc39d6a8
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-13 10:18:14 +0100

    Update app

--- read_button.py
+++ read_button.py
@@ -4,25 +4,21 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-INPUT_PIN2 = 20
+INPUT_PIN = 12
 
-GPIO.setup(INPUT_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 while True:
 
-    pin2 = GPIO.input(INPUT_PIN2) == True
+    pin1 = GPIO.input(INPUT_PIN) == False
 
-    if (pin2):
+    if (pin1):
         with open('/tmp/lampen', 'w') as f:
             f.write('')
         with open('/tmp/armen', 'w') as f:
             f.write('')
         with open('/tmp/zwaailicht', 'w') as f:
             f.write('')
-
-#          wait until move sensor is off
-        while GPIO.input(INPUT_PIN2):
-            sleep(0.2);
 
     sleep(0.2);
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 /usr/bin/python /home/pi/ferry50/zwaailicht.py&
+/usr/bin/python /home/pi/ferry50/lampen.py&
 /usr/bin/python /home/pi/ferry50/read_sensors.py&
+/usr/bin/python /home/pi/ferry50/read_button.py&
 #/usr/bin/python /home/pi/ferry50/abraham.py&
 #/usr/bin/python /home/pi/ferry50/test2.py&
 
