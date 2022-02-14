5b07891064c2afae360474614c0a67b9d2ca6dbf
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-14 10:01:06 +0100

    Update app

--- test_inputs.py
+++ test_inputs.py
@@ -7,11 +7,14 @@ GPIO.setmode(GPIO.BCM)
 INPUT_PIN = 12
 INPUT_PIN2 = 20
 
+GPIO.setup(INPUT_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 while True:
 
     pin1 = GPIO.input(INPUT_PIN) == False
     pin2 = GPIO.input(INPUT_PIN2) == True
-    
+
     print('pin1: ', pin1)
     print('pin2: ', pin2)
 
