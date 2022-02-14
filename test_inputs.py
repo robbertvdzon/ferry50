9099af4d14b50efb29e366862be1734699f4f8e6
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-14 20:11:08 +0100

    Update app

--- test_inputs.py
+++ test_inputs.py
@@ -7,8 +7,8 @@ GPIO.setmode(GPIO.BCM)
 INPUT_PIN = 12
 INPUT_PIN2 = 20
 
-GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(INPUT_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_PIN, GPIO.IN)
+GPIO.setup(INPUT_PIN2, GPIO.IN)
 
 while True:
 
