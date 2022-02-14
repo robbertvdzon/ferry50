973cd6ec6ad5e2f8a3fd5cad6545f03147a52077
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-14 20:16:38 +0100

    Update app

--- test_inputs.py
+++ test_inputs.py
@@ -10,12 +10,28 @@ INPUT_PIN2 = 20
 GPIO.setup(INPUT_PIN, GPIO.IN)
 GPIO.setup(INPUT_PIN2, GPIO.IN)
 
+RELAIS = 16
+RELAIS2 = 26 # naar led
+GPIO.setup(RELAIS, GPIO.OUT)
+GPIO.setup(RELAIS2, GPIO.OUT)
+
+
 while True:
 
     pin1 = GPIO.input(INPUT_PIN) == False
     pin2 = GPIO.input(INPUT_PIN2) == True
 
+    if (pin1):
+        GPIO.output(RELAIS, 0)
+    else
+        GPIO.output(RELAIS, 1)
+
+    if (pin2):
+        GPIO.output(RELAIS, 0)
+    else
+        GPIO.output(RELAIS, 1)
+
     print('pin1: ', pin1)
-#     print('pin2: ', pin2)
+    print('pin2: ', pin2)
 
     sleep(1);
