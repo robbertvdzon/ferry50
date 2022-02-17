b10a66f2c9435baa03878cb2c897a7bda246891f
robbertvdzon <robbert.vanderzon@wearetriple.com>
2022-02-17 18:40:14 +0100

    Update app

--- read_sensors.py
+++ read_sensors.py
@@ -17,7 +17,7 @@ while True:
 
     if (pin2):
         print('aan ', datetime.now().time())
-        if (!lastStatus){
+        if (not lastStatus){
             addLog('movement detected')
             with open('/tmp/lampen', 'w') as f:
                 f.write('')
